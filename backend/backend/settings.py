266831a39f5139a4e89353691b59850c374aa14d
Lada Kolotova <ladakolotova@MacBook-Pro-3.local>
2024-04-25 12:56:20 +0300

    Correct syte name.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['ladank.sytes.net']
+ALLOWED_HOSTS = ['taski-ladank.sytes.net']
 
 
 # Application definition
