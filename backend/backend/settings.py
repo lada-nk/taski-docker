30124414dbcf638665d7d07e8aa20ca501af704f
Lada Kolotova <ladakolotova@MacBook-Pro-3.local>
2024-04-27 22:17:28 +0300

    Settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,12 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['taski-ladank.sytes.net']
-
 
 # Application definition
 
